2581eca057b82e3c3bdc910e9bb443e1055fb777
baptou12 <baptiste@spotistic.com>
2015-04-28 14:20:15 +0200

    Updated: lines' color

--- SPLockScreen/SPLockOverlay.m
+++ SPLockScreen/SPLockOverlay.m
@@ -8,8 +8,8 @@
 
 #import "SPLockOverlay.h"
 
-#define kLineColor			[UIColor colorWithRed:255.0/255.0 green:252.0/255.0 blue:78.0/255.0 alpha:0.9]
-#define kLineGridColor  [UIColor colorWithRed:255.0/255.0 green:252.0/255.0 blue:233.0/255.0 alpha:1.0]
+#define kLineColor	[UIColor colorWithRed:22.0/255.0 green:160.0/255.0 blue:133.0/255.0 alpha:0.6]
+#define kLineGridColor  [UIColor colorWithRed:255.0/255.0 green:255.0/255.0 blue:255.0/255.0 alpha:1.0]
 
 @implementation SPLockOverlay
 
